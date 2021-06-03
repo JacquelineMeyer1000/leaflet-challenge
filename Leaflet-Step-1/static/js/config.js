d2a175cd0c326a097f5bc35db631fc64572d5eed
Jacqueline Meyer <jacquelinemeye1000@gmail.com>
2021-06-03 12:21:02 -0500

    leaflet map is working

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiamFjcXVlbGluZW1leWVyMTAwMCIsImEiOiJja3BoNTNwY3AwMGFtMnVscmp1YTFkZm1uIn0.Qucvz08szvQrKCptVfyjxg";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,16 @@
+var myMap = L.map("mapid", {
+    center: [37.09, -95.71],
+    zoom: 4
+  });
+  
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
